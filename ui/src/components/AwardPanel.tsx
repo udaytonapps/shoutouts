e462d1ca9661be6b303dd5b59cbca6a47ce45ba6
Andrew Niekamp <aniekamp1@udayton.edu>
2022-10-17 10:38:59 -0400

    TSUGI-430 Add award panel

---
+++ ui/src/components/AwardPanel.tsx
@@ -0,0 +1,56 @@
+import { MoreHoriz, WorkspacePremium } from "@mui/icons-material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import { LearnerAward } from "../utils/types";
+
+interface AwardPanelProps {
+  award: LearnerAward;
+}
+
+/** Show basic AwardPanel info */
+export default function AwardPanel(props: AwardPanelProps) {
+  const { award } = props;
+  return (
+    <Card>
+      <CardHeader
+        avatar={<WorkspacePremium fontSize="large" />}
+        action={
+          <IconButton aria-label="settings">
+            <MoreHoriz />
+          </IconButton>
+        }
+        title={<Typography fontWeight={"bold"}>{award.label}</Typography>}
+      />
+      <CardContent>
+        <Box display={"flex"}>
+          <Box display={"flex"}>
+            <img
+              width={125}
+              height={125}
+              style={{ objectFit: "cover" }}
+              src={process.env.PUBLIC_URL + award.imageUrl}
+              alt={award.label}
+            />
+          </Box>
+          <Box
+            pl={5}
+            display={"flex"}
+            flexDirection={"column"}
+            justifyContent={"space-around"}
+          >
+            <Typography>{award.description}</Typography>
+            <Typography variant="body2" fontStyle={"italic"}>
+              {award.comment}
+            </Typography>
+          </Box>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
