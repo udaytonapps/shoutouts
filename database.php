c5f1401121cf27d36f469c5f9ba0ae7890b3e346
Andrew Niekamp <aniekamp1@udayton.edu>
2022-10-20 09:47:26 -0400

    TSUGI-433 Update db schema for config and exclusions

--- database.php
+++ database.php
@@ -4,15 +4,47 @@
 $DATABASE_UNINSTALL = array();
 
 /** Table names */
+$AWARDS_CONFIGURATION_TABLE_NAME = "{$CFG->dbprefix}awards_configuration";
 $AWARDS_INSTANCE_TABLE_NAME = "{$CFG->dbprefix}awards_instance";
 $AWARDS_TYPE_TABLE_NAME = "{$CFG->dbprefix}awards_type";
+$AWARDS_TYPE_EXCLUSION_TABLE_NAME = "{$CFG->dbprefix}awards_type_exclusion";
 
 /** Table schemas */
+
+$AWARDS_CONFIGURATION = "CREATE TABLE {$AWARDS_CONFIGURATION_TABLE_NAME} (
+
+    /* PRIMARY KEY */
+    configuration_id        INTEGER NOT NULL AUTO_INCREMENT,
+    
+    /* COMMON COLS */
+    context_id              INTEGER, /* Tracked and scoped, this is the course */
+    link_id                 INTEGER, /* Tracked but not scoped, this is the instance */
+    created_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
+    /* CONFIGURATION COLS */
+    comments_required       BOOLEAN NOT NULL,
+    moderation_enabled      BOOLEAN NOT NULL,
+    anonymous_enabled       BOOLEAN NOT NULL,
+    recipient_view_enabled  BOOLEAN NOT NULL,
+    notifications_enabled   BOOLEAN NOT NULL,
+    leaderboard_enabled     BOOLEAN NOT NULL,
+    awarded_value           INTEGER, -- NULLABLE?
+    awarded_limit           INTEGER, -- NULLABLE?
+    awarded_cooldown        INTEGER, -- NULLABLE?
+    received_value          INTEGER, -- NULLABLE?
+    received_limit          INTEGER, -- NULLABLE?
+    received_cooldown        INTEGER, -- NULLABLE?
+
+
+    PRIMARY KEY(configuration_id)
+
+) ENGINE = InnoDB DEFAULT CHARSET=utf8";
 
 $AWARDS_INSTANCE = "CREATE TABLE {$AWARDS_INSTANCE_TABLE_NAME} (
 
     /* PRIMARY KEY */
-    award_instance_id             INTEGER NOT NULL AUTO_INCREMENT,
+    award_instance_id       INTEGER NOT NULL AUTO_INCREMENT,
     
     /* COMMON COLS */
     context_id              INTEGER NOT NULL, /* Tracked and scoped, this is the course */
@@ -46,16 +78,37 @@ $AWARDS_TYPE = "CREATE TABLE {$AWARDS_TYPE_TABLE_NAME} (
     image_url               TEXT NOT NULL,
     label                   TEXT NOT NULL,
     short_description       TEXT NOT NULL,
+    is_active               BOOLEAN DEFAULT 1, /* Defaulting to active */
 
     PRIMARY KEY(award_type_id)
 
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8";
 
+$AWARDS_TYPE_EXCLUSION = "CREATE TABLE {$AWARDS_TYPE_EXCLUSION_TABLE_NAME} (
+
+    /* PRIMARY KEY */
+    exclusion_id            INTEGER NOT NULL AUTO_INCREMENT,
+    
+    /* COMMON COLS */
+    context_id              INTEGER, /* Tracked and scoped, this is the course */
+    link_id                 INTEGER, /* Tracked but not scoped, this is the instance */
+    created_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
+    /* EXCLUSION COLS */
+    configuration_id        INTEGER NOT NULL, /* Indication of the related configuration */
+    award_type_id           INTEGER NOT NULL, /* Indication of the award to be excluded */
+
+    PRIMARY KEY(exclusion_id)
+
+) ENGINE = InnoDB DEFAULT CHARSET=utf8";
+
 /** Table installation (if tables don't exist) */
 $DATABASE_INSTALL = array(
-    // array($AWARDS_SETTINGS_TABLE_NAME, $AWARDS_SETTINGS),
+    array($AWARDS_CONFIGURATION_TABLE_NAME, $AWARDS_CONFIGURATION),
     array($AWARDS_INSTANCE_TABLE_NAME, $AWARDS_INSTANCE),
-    array($AWARDS_TYPE_TABLE_NAME, $AWARDS_TYPE)
+    array($AWARDS_TYPE_TABLE_NAME, $AWARDS_TYPE),
+    array($AWARDS_TYPE_EXCLUSION_TABLE_NAME, $AWARDS_TYPE_EXCLUSION)
 );
 
 $DATABASE_UPGRADE = function ($oldversion) {
