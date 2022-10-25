b7cbcdbe9f9f1179807c66efb858f3d3935d2171
Andrew Niekamp <aniekamp1@udayton.edu>
2022-10-25 15:18:59 -0400

    TSUGI-426, TSUGI-427 Completely update instructor view

--- ui/src/views/InstructorView.tsx
+++ ui/src/views/InstructorView.tsx
@@ -1,30 +1,256 @@
-import { Box } from "@mui/material";
+import { Box, Tab, Tabs, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import AlertPanel from "../components/AlertPanel";
-import CommentsTable from "../components/CommentsTable";
-import { getCourseComments } from "../utils/api-connector";
-import { TemplateComment } from "../utils/types";
+import TabPanel from "../components/common/TabPanel";
+import HistoryTable from "../components/HistoryTable";
+import LeaderboardTable from "../components/LeaderboardTable";
+import PendingTable from "../components/PendingTable";
+import ReviewDialog from "../components/ReviewDialog";
+import {
+  getAllAwards,
+  getAllAwardsHistory,
+  getAllPendingAwards,
+  getContextConfiguration,
+  updateAward,
+} from "../utils/api-connector";
+import { a11yProps, compareLastNames } from "../utils/common/helpers";
+import {
+  AllAwardsTableRecord,
+  AwardsConfiguration,
+  AwardStatusUpdateData,
+  HistoryTableRow,
+  PendingTableRow,
+  RequestStatus,
+} from "../utils/types";
 
 function InstructorView() {
-  const [comments, setComments] = useState<TemplateComment[]>([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [configuration, setConfiguration] = useState<AwardsConfiguration>();
+  const [tabPosition, setTabPosition] = useState(0);
+  const [reviewDialogOpen, setReviewDialogOpen] = useState(false);
+  const [allAwards, setAllAwards] = useState<AllAwardsTableRecord[]>([]);
+  const [pendingAwards, setPendingAwards] = useState<PendingTableRow[]>([]);
+  const [historyAwards, setHistoryAwards] = useState<HistoryTableRow[]>([]);
+  const [awardToReview, setAwardToReview] = useState<
+    HistoryTableRow | PendingTableRow
+  >();
+
+  const tabs = ["AWARDED", "HISTORY"];
+  if (configuration?.moderation_enabled) {
+    tabs.unshift("PENDING");
+  }
 
   useEffect(() => {
-    setLoading(true); // Turn loading indicator on
-    getCourseComments().then((comments) => {
-      setComments(comments);
-      setLoading(false); // Turn loading indicator off
+    // Retrieve the list of alerts to display
+    getContextConfiguration().then((config) => {
+      if (config) {
+        setConfiguration(config);
+        // Assemble the promises to run them all in parallel
+        const promises = [fetchAwarded(), fetchPending(), fetchHistory()];
+        // Turn off loading once all promises are settled
+        return Promise.allSettled(promises).then(() => {
+          setLoading(false);
+        });
+      }
     });
+    // The empty dependency array '[]' means this will run once, when the component renders
   }, []);
+
+  const fetchPending = async () => {
+    const awards = await getAllPendingAwards();
+    setPendingAwards(awards);
+  };
+
+  const fetchAwarded = async () => {
+    const awards = await getAllAwards();
+    setAllAwards(awards);
+  };
+
+  const fetchHistory = async () => {
+    const awards = await getAllAwardsHistory();
+    setHistoryAwards(awards);
+  };
+
+  // Tab management
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    refreshTabs();
+    setTabPosition(newValue);
+  };
+
+  const refreshTabs = () => {
+    setLoading(true);
+    if (configuration) {
+      Promise.allSettled([fetchAwarded(), fetchPending(), fetchHistory()]).then(
+        () => {
+          setLoading(false);
+        }
+      );
+    }
+  };
+
+  // Dialog management
+  const handleOpenReviewDialogFromHistory = (reviewId: string) => {
+    setAwardToReview(historyAwards.find((award) => award.id === reviewId));
+    setReviewDialogOpen(true);
+  };
+
+  const handleOpenReviewDialogFromPending = (reviewId: string) => {
+    setAwardToReview(pendingAwards.find((award) => award.id === reviewId));
+    setReviewDialogOpen(true);
+  };
+
+  const handleCloseReviewDialog = (event?: object, reason?: string) => {
+    const reasonsToStayOpen = ["backdropClick", "escapeKeyDown"];
+    if (reason && reasonsToStayOpen.includes(reason)) {
+      return;
+    }
+    setReviewDialogOpen(false);
+  };
+
+  const handleSaveReviewDialog = async (reviewData: AwardStatusUpdateData) => {
+    // Close the dialog
+    setReviewDialogOpen(false);
+    // Send the update
+    await updateAward(reviewData);
+    // Refresh the data in the UI
+    refreshTabs();
+  };
 
   return (
     <>
-      <AlertPanel />
-      <Box pt={1}>
-        <CommentsTable loading={loading} rows={comments} />
-      </Box>
+      {configuration && (
+        <Box>
+          <Box pt={6} sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs
+              value={tabPosition}
+              variant="scrollable"
+              onChange={handleTabChange}
+              aria-label="basic tabs example"
+            >
+              {tabs.map((tab, i) => {
+                return (
+                  <Tab
+                    key={`tab-${tab}`}
+                    label={
+                      <Typography pb={0.5} variant="body2">
+                        {tab}
+                      </Typography>
+                    }
+                    {...a11yProps(i)}
+                  />
+                );
+              })}
+            </Tabs>
+          </Box>
+          {tabs.map((tab, i) => {
+            return (
+              <TabPanel key={`tab-panel-${tab}`} value={tabPosition} index={i}>
+                {tab === "PENDING" && (
+                  <>
+                    <PendingTable
+                      rows={pendingAwards}
+                      loading={loading}
+                      filters={pendingTableFilters}
+                      openReviewDialog={handleOpenReviewDialogFromPending}
+                    />
+                  </>
+                )}
+                {tab === "AWARDED" && (
+                  <>
+                    <LeaderboardTable
+                      configuration={configuration}
+                      rows={allAwards}
+                      loading={loading}
+                      filters={awardsTableFilters}
+                      sorting={true}
+                    />
+                  </>
+                )}
+                {tab === "HISTORY" && (
+                  <>
+                    <HistoryTable
+                      configuration={configuration}
+                      rows={historyAwards}
+                      loading={loading}
+                      filters={historyTableFilters}
+                      openReviewDialog={handleOpenReviewDialogFromHistory}
+                    />
+                  </>
+                )}
+              </TabPanel>
+            );
+          })}
+          <ReviewDialog
+            configuration={configuration}
+            handleClose={handleCloseReviewDialog}
+            handleSave={handleSaveReviewDialog}
+            open={reviewDialogOpen}
+            requestRow={awardToReview || null}
+          />
+        </Box>
+      )}
     </>
   );
 }
+
+const awardsTableFilters = [
+  {
+    column: "lastFirst",
+    label: "Recipient Name",
+    type: "enum",
+    sort: compareLastNames,
+  },
+];
+
+const pendingTableFilters = [
+  {
+    column: "senderName",
+    label: "Sender Name",
+    type: "enum",
+    sort: compareLastNames,
+  },
+  {
+    column: "recipientName",
+    label: "Recipient Name",
+    type: "enum",
+    sort: compareLastNames,
+  },
+  {
+    column: "label",
+    label: "Award Type",
+    type: "enum",
+  },
+];
+
+const historyTableFilters = [
+  {
+    column: "senderName",
+    label: "Sender Name",
+    type: "enum",
+    sort: compareLastNames,
+  },
+  {
+    column: "recipientName",
+    label: "Recipient Name",
+    type: "enum",
+    sort: compareLastNames,
+  },
+  {
+    column: "label",
+    label: "Award Type",
+    type: "enum",
+  },
+  {
+    column: "status",
+    label: "Status",
+    type: "enum",
+    valueMapping: (val: RequestStatus) => {
+      if (val === "SUBMITTED") {
+        return "Pending";
+      } else {
+        return val.charAt(0).toUpperCase() + val.slice(1).toLowerCase();
+      }
+    },
+  },
+];
 
 export default InstructorView;
